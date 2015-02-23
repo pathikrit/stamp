e7f626b1432734b43d937596da687c49f99e9a89
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-02-22 20:42:39 -0800

    use datatables for testing

--- src/test/scala/com/github/pathikrit/stamp/FormatLikeSpec.scala
+++ src/test/scala/com/github/pathikrit/stamp/FormatLikeSpec.scala
@@ -2,25 +2,23 @@ package com.github.pathikrit.stamp
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAccessor
 
+import org.specs2.matcher.DataTables
 import org.specs2.mutable.Specification
 
-class FormatLikeSpec extends Specification {
+class FormatLikeSpec extends Specification with DataTables { override def is =
+  "Work for valid examples" ! positiveSpec
 
-  private[this] implicit class StringImplicits(s: String) {
-    def mustFormatTo(output: String)(implicit t: TemporalAccessor) = {
-      val pattern = FormatLike(s)
-      DateTimeFormatter.ofPattern(pattern).format(t) aka pattern mustEqual output
-    }
-  }
+  def asDateTimeFormatter(example: String) = DateTimeFormatter.ofPattern(FormatLike(example))
 
-  "stamp" should {
-    "parse human examples" in {
-      implicit val date = LocalDateTime.of(1986, 8, 25, 11, 23, 32, 19)
-      "2013 AD" mustFormatTo "1986 AD"
-      "1500 Anno Domini" mustFormatTo "1986 Anno Domini"
-      "Jan 26, 1926" mustFormatTo "Aug 25, 1986"
-    }
+  val t1 = LocalDateTime.of(1986, 8, 25, 11, 23, 32, 19)
+
+  def positiveSpec =
+  "example"                                         || "time"       | "output"                                        |
+  "2013 AD"                                         !! t1           ! "1986 AD"                                       |
+  "1500 Anno Domini"                                !! t1           ! "1986 Anno Domini"                              |
+  "Jan 26, 1926"                                    !! t1           ! "Aug 25, 1986"                                  |>
+  {
+    (example, time, output) => asDateTimeFormatter(example).format(time) aka FormatLike(example) mustEqual output
   }
 }
