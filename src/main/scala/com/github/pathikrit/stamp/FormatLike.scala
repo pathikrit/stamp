3ffa74ee17e7b78441fa7ddcff415dde7e576bc2
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-02-22 20:42:58 -0800

    add range checks

--- src/main/scala/com/github/pathikrit/stamp/FormatLike.scala
+++ src/main/scala/com/github/pathikrit/stamp/FormatLike.scala
@@ -2,9 +2,14 @@ package com.github.pathikrit.stamp
 
 import java.text.DateFormatSymbols
 
+import scala.util.Try
 import scala.util.matching.Regex, Regex.Match
 
-private[stamp] class Rule(pattern: String, matchRegexes: Seq[String], check: String => Boolean = _ => true) {
+private[stamp] case class Rule(pattern: String, matchRegexes: Seq[String], check: String => Boolean = _ => true) {
+
+  def addCheck(f: Int => Boolean) = this.copy(check = (snippet) => check(snippet) && Try(snippet.toInt).map(f).getOrElse(false))
+
+  def addRangeCheck(r: Range) = addCheck(r.contains)
 
   val matchRegex = matchRegexes.mkString("(", ")|(", ")").r     // join them using an OR
 
@@ -16,7 +21,7 @@ private[stamp] class Rule(pattern: String, matchRegexes: Seq[String], check: Str
 private[stamp] object Rule {
   import scala.collection.JavaConversions._
 
-  private[this] implicit class StringImplicits(s: String) {
+  implicit class StringImplicits(s: String) {
     def word = s"(?i)\\b$s\\b"
   }
 
@@ -34,14 +39,15 @@ private[stamp] object Rule {
   val year2 = Rule("YY")
   val year4 = Rule("YYYY")
 
-  val month = Rule("M")
-  val month2 = Rule("MM")
+  val month = Rule("M").addRangeCheck(1 to 12)
+  val month2 = Rule("MM").addRangeCheck(1 to 12)
   val month3 = Rule("MMM", _.getShortMonths)
   val month4 = Rule("MMMM", _.getMonths)
 
-  val dayOfMonth = Rule("d")
-  val dayOfMonth2 = Rule("dd")
+  val dayOfMonth = Rule("d").addRangeCheck(1 to 31)
+  val dayOfMonth2 = Rule("dd").addRangeCheck(1 to 31)
 
+// TODO: Q1 vs 3rd of quarter vs 03 vs 3???
 //  val quarter = "QQ"
 //  val fullQuarter = "QQQQ"
 
@@ -50,18 +56,37 @@ private[stamp] object Rule {
 
   val amPm = Rule("a", _.getAmPmStrings)
 
-  val hourOfDayAmPm = Rule("h")
-  val hourOfDayAmPm2 = Rule("hh")
+  val hourOfDayAmPm = Rule("h").addRangeCheck(1 to 12)
+  val hourOfDayAmPm2 = Rule("hh").addRangeCheck(1 to 12)
 
-  val hourOfDay = Rule("H")
-  val hourOfDay2 = Rule("HH")
+  val hourOfDay = Rule("H").addRangeCheck(0 to 23)
+  val hourOfDay2 = Rule("HH").addRangeCheck(0 to 23)
 
-  val minuteOfHour = Rule("m")
-  val minuteOfHour2 = Rule("mm")
+  val minuteOfHour = Rule("m").addRangeCheck(0 to 59)
+  val minuteOfHour2 = Rule("mm").addRangeCheck(0 to 59)
 
-  val secondOfMinute = Rule("s")
-  val secondOfMinute2 = Rule("ss")
+  val secondOfMinute = Rule("s").addRangeCheck(0 to 59)
+  val secondOfMinute2 = Rule("ss").addRangeCheck(0 to 59)
 
+  // TODO: Support these
+  /**
+  S       fraction-of-second          fraction          978
+   A       milli-of-day                number            1234
+   n       nano-of-second              number            987654321
+   N       nano-of-day                 number            1234000000
+
+   V       time-zone ID                zone-id           America/Los_Angeles; Z; -08:30
+   z       time-zone name              zone-name         Pacific Standard Time; PST
+   O       localized zone-offset       offset-O          GMT+8; GMT+08:00; UTC-08:00;
+   X       zone-offset 'Z' for zero    offset-X          Z; -08; -0830; -08:30; -083015; -08:30:15;
+   x       zone-offset                 offset-x          +0000; -08; -0830; -08:30; -083015; -08:30:15;
+   Z       zone-offset                 offset-Z          +0000; -0800; -08:00;
+
+   '       escape for text             delimiter
+   ''      single quote                literal           '
+   [       optional section start
+   ]       optional section end
+    */
 }
 
 object FormatLike {
@@ -80,23 +105,3 @@ object FormatLike {
     Rule.month4 |> Rule.month3 |> Rule.dayOfMonth
 }
 
-/**
-   Q/q     quarter-of-year             number/text       3; 03; Q3; 3rd quarter
-
-   S       fraction-of-second          fraction          978
-   A       milli-of-day                number            1234
-   n       nano-of-second              number            987654321
-   N       nano-of-day                 number            1234000000
-
-   V       time-zone ID                zone-id           America/Los_Angeles; Z; -08:30
-   z       time-zone name              zone-name         Pacific Standard Time; PST
-   O       localized zone-offset       offset-O          GMT+8; GMT+08:00; UTC-08:00;
-   X       zone-offset 'Z' for zero    offset-X          Z; -08; -0830; -08:30; -083015; -08:30:15;
-   x       zone-offset                 offset-x          +0000; -08; -0830; -08:30; -083015; -08:30:15;
-   Z       zone-offset                 offset-Z          +0000; -0800; -08:00;
-
-   '       escape for text             delimiter
-   ''      single quote                literal           '
-   [       optional section start
-   ]       optional section end
- */
